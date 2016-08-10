dfa1b89c43c519175f31c7257b56f947acd7dfac
Jacob Vigeveno <vigevenoj@gmail.com>
2016-08-10 06:52:16 -0700

    Stop printing debugging info

--- src/main/java/com/sharkbaitextraordinaire/sprungrun/SprungrunApplication.java
+++ src/main/java/com/sharkbaitextraordinaire/sprungrun/SprungrunApplication.java
@@ -4,15 +4,13 @@ package com.sharkbaitextraordinaire.sprungrun;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.jdbc.core.JdbcTemplate;
 
-import com.sharkbaitextraordinaire.sprungrun.model.Run;
 
 @SpringBootApplication
-public class SprungrunApplication implements CommandLineRunner {
+public class SprungrunApplication {
 
     private static final Logger log = LoggerFactory.getLogger(SprungrunApplication.class);
 
@@ -22,31 +20,4 @@ public class SprungrunApplication implements CommandLineRunner {
 	public static void main(String[] args) {
 		SpringApplication.run(SprungrunApplication.class, args);
 	}
-
-
-	// this is a test class to print any runs
-	@Override
-	public void run(String... strings) throws Exception {
-		log.info("querying for runs");
-		jdbcTemplate.query(
-				"select "
-				+ "rdate, timeofday, distance, units, elapsed, effort, comment, shoeid "
-				+ "from runs "
-				+ "where rdate >= '2014-12-01'", 
-				(rs, rowNum) -> { 
-				Run run = new Run();
-				run.setRdate(rs.getDate("rdate").toLocalDate());
-				run.setTimeofday(rs.getString("timeofday"));
-				run.setDistance(rs.getBigDecimal("distance"));
-				run.setUnits(rs.getString("units"));
-				run.setElapsed(rs.getTime("elapsed"));
-				run.setEffort(rs.getString("effort"));
-				run.setComment(rs.getString("comment"));
-				run.setShoeid(rs.getInt("shoeid"));
-				return run;
-				}).forEach(run -> log.info(run.toString()));
-		log.info("done printing run info");
-	}
-	
-	
 }
