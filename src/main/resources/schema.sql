1745be6af5de9ecd46e677b0ebf84e7deb3b08fb
Jacob Vigeveno <vigevenoj@gmail.com>
2016-11-07 22:19:22 -0800

    Moved schema.sql to classpath

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,18 +1,18 @@
 create table runs (
-  runid int(11) default null,
+  runid integer default null,
   rdate date default null,
   timeofday varchar(4) default null,
   distance decimal(6,2),
   units varchar(5),
   elapsed time,
   effort varchar(50),
-  'comment' varchar(200),
-  shoeid int(11)
+  comments varchar(200),
+  shoeid integer
 );
 
 create table unit_conversion (
   from_u varchar(5),
   to_u varchar(5),
-  factor double
+  factor double precision
 );
 
