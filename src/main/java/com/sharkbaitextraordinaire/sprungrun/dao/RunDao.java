8fe6ad5ecc8d888b1e76baf0e990f2894047bdb1
Jacob Vigeveno <jacob.vigeveno@jivesoftware.com>
2016-11-10 21:15:16 -0800

    Add method to get runs faster than a given pace

--- src/main/java/com/sharkbaitextraordinaire/sprungrun/dao/RunDao.java
+++ src/main/java/com/sharkbaitextraordinaire/sprungrun/dao/RunDao.java
@@ -1,8 +1,12 @@
 package com.sharkbaitextraordinaire.sprungrun.dao;
 
 import java.sql.Date;
+import java.time.Duration;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.TimeZone;
+import java.util.Locale;
+import java.text.SimpleDateFormat;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
@@ -16,6 +20,12 @@ public class RunDao {
 	
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
+
+  private String formatDuration(Duration duration) {
+    SimpleDateFormat hhmmssFormat = new SimpleDateFormat("HH:mm:ss", Locale.getDefault());
+    hhmmssFormat.setTimeZone(TimeZone.getTimeZone("GMT+0"));
+    return hhmmssFormat.format(duration);
+  }
 	
 	public void insert(Run run) {
 		String sql = "insert into runs (rdate, timeofday, distance, units, elapsed, effort, comments, shoeid)"
@@ -48,4 +58,22 @@ public class RunDao {
 				return run;}, Date.valueOf(date));
 		return runs;
 	}
+
+  public List<Run> fasterThanPace(Duration pace, String units) {
+    String sql = "select rdate, timeofday, distance, units, elapsed, effort, comments, shoeid from runs"
+      + " "; // TODO get pace query
+    List<Run> runs = jdbcTemplate.query(sql,
+        (rs, rowNum) -> {
+          Run run = new Run();
+          run.setRdate(rs.getDate("rdate").toLocalDate());
+				run.setTimeofday(rs.getString("timeofday"));
+				run.setDistance(rs.getBigDecimal("distance"));
+				run.setUnits(rs.getString("units"));
+				run.setElapsed(rs.getTime("elapsed"));
+				run.setEffort(rs.getString("effort"));
+				run.setComment(rs.getString("comment"));
+				run.setShoeid(rs.getInt("shoeid"));
+				return run;}, formatDuration(pace));
+		return runs;
+  }
 }
